a4bf70348f5dc9c0c67e9454c962307bda7482ad
prasannabe2004 <prasannabe2004@gmail.com>
2016-06-19 19:01:13 -0700

    To strip warning for auto usage, enable c++1y compiler flag. To use nullptr, use c++1y flag.

---
+++ mystuffs/codelite/nullptr/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	int* ptr = nullptr;
+	
+	cout << "hello world\n" << endl;
+	return 0;
+}
--- mystuffs/codelite/nullptr/nullptr.mk
+++ mystuffs/codelite/nullptr/nullptr.mk
@@ -3,10 +3,10 @@
 ## any manual changes will be erased      
 ##
 ## Debug
-ProjectName            :=auto
+ProjectName            :=nullptr
 ConfigurationName      :=Debug
 WorkspacePath          := "/home/prasanna/codelite"
-ProjectPath            := "/home/prasanna/codelite/Cpp/mystuffs/codelite/auto"
+ProjectPath            := "/home/prasanna/codelite/Cpp/mystuffs/codelite/nullptr"
 IntermediateDirectory  :=./Debug
 OutDir                 := $(IntermediateDirectory)
 CurrentFileName        :=
@@ -32,7 +32,7 @@ Preprocessors          :=
 ObjectSwitch           :=-o 
 ArchiveOutputSwitch    := 
 PreprocessOnlySwitch   :=-E
-ObjectsFileList        :="auto.txt"
+ObjectsFileList        :="nullptr.txt"
 PCHCompileFlags        :=
 MakeDirCommand         :=mkdir -p
 LinkOptions            :=  
@@ -92,7 +92,7 @@ PreBuild:
 ## Objects
 ##
 $(IntermediateDirectory)/main.cpp$(ObjectSuffix): main.cpp $(IntermediateDirectory)/main.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/prasanna/codelite/Cpp/mystuffs/codelite/auto/main.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/main.cpp$(ObjectSuffix) $(IncludePath)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/prasanna/codelite/Cpp/mystuffs/codelite/nullptr/main.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/main.cpp$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/main.cpp$(DependSuffix): main.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/main.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/main.cpp$(DependSuffix) -MM "main.cpp"
 
